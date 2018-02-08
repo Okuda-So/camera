f4e7a82edc7fdc04e8dbd736df1f3257d2fe8e04
Okuda-So <32082939+Okuda-So@users.noreply.github.com>
2018-02-08 19:14:24 +0900

    Update ROS_camera.py

--- ROS/device/ROS_camera.py
+++ ROS/device/ROS_camera.py
@@ -6,7 +6,7 @@ import threading
 import os
 import glob
 import sys
-sys.path.append(“/opt/ros/kinetic/lib/python2.7/dist-packages”)
+sys.path.append("/opt/ros/kinetic/lib/python2.7/dist-packages")
 import cv2
 import numpy
 import camera
@@ -18,15 +18,17 @@ from sensor_msgs.msg import Image
 from necst.msg import oneshot_msg
 
 class camera_con(object):
-    filename = ‘’
+    filename = ''
     filesize = 1
-
+    
+    
     def __init__(self):
-        #camera = camera.controller()
-        #camera.detect_camera()
-        #camera.setwhitebalance(white=’SKY’)
-        #camera.set_crop(crop=‘1.3x’)
-        pass
+        self.DLSR = camera.controller()
+        self.DLSR.detect_camera()
+        self.DLSR.set_whitebalance(white='SKY')
+        self.DLSR.set_crop(crop='1.3x')
+        #pass
+	return
 
     def start_thread(self):
         th = threading.Thread(target = self.pub_image)
@@ -39,35 +41,34 @@ class camera_con(object):
         self.filesize = req.filesize
         print(req)
         while True:
-            if not self.filename == ‘’:
+            if not self.filename == '':
                 break
-            print(‘please filename!’)
-        #image = cam.shutter_download(filename=self.filename)
+            print('please filename!')
+        image = self.DLSR.shutter_download(filename=self.filename)
         self.pub_image()
-        print(‘shoot’+self.filename)
-        return
+        print('shoot'+self.filename)
+        return 
 
     def pub_image(self):
         while True:
-            if os.path.exists(self.filename) == True:
+            if os.path.exists(self.filename) == True :
                 break
             time.sleep(0.1)
-        print(“$$$$$$$$$$$$”)
+        print("$$$$$$$$$$$$$")
         img = cv2.imread(self.filename, 1)
         bridge = CvBridge()
-        pub = rospy.Publisher(‘Image’, Image, queue_size = 100, latch = True)
+        pub = rospy.Publisher('Image', Image, queue_size = 1, latch = True)
 
         while not rospy.is_shutdown():
-            pub.publish(bridge.cv2_to_imgmsg(omg, ‘bgr8’))
-            print(‘publish’)
+            pub.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
+            print('publish')
             time.sleep(0.5)
         return
 
-if __name__ == ‘__main__’:
+if __name__ == '__main__':
     cam = camera_con()
-    rospy.init_node(‘camera_controller’)
-    rospy.loginfo(‘waiting publish camera’)
+    rospy.init_node('camera_controller')
+    rospy.loginfo('waiting publish camera')
     cam.start_thread()
-    sub = rospy.Subscriber(‘oneshot’, oneshot_msg, cam.shutter)
+    sub = rospy.Subscriber('oneshot', oneshot_msg, cam.shutter)
     rospy.spin()
- 
